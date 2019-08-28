6e64ef90f4c9fc6d551e71d21bc0e15d220b7efe
jlayog <jerel.layog@gmail.com>
2019-08-28 14:30:43 -0400

    FINALLY FUCKING SOLVED IT
    I was invoking a function before, and not adding it to the newly created functions. Have refactored some code for this to work.
    
    Currently looking for a cleaner solution, but for now, that'll do, pig.

--- toDoApp/app.js
+++ toDoApp/app.js
@@ -10,8 +10,6 @@ done();
 remove();
 
 
-
-
 //handle adding task via input
 input.addEventListener("keypress", function(e) {
     //function starts only upon hitting the enter key
@@ -26,35 +24,39 @@ input.addEventListener("keypress", function(e) {
         createLi.innerHTML = "<span>X</span> " + todoText;
         // append to ul to make li
         document.querySelector("ul").appendChild(createLi);
-        // Next problem, how to add Event listeners to all future events
-        // https://stackoverflow.com/questions/14258787/add-event-listener-on-elements-created-dynamically
+        // adding the eventlisteners to dynamically created elements.
+        createLi.addEventListener("click", linethrough);
+        createLi.addEventListener("click", fadeOut);
     }
-})
+});
 
 
 //changing jquery fadeOut and remove to javascript
 function remove() {
     for (var i = 0; i < span.length; i++) {
-        span[i].addEventListener("click", function() {    
-            if(event.target.localName === "span") {
-                var removeLi = event.target.parentNode;
-                // fadeout css
-                removeLi.classList.add("fadeOut");
-                // wait 600ms then remove li
-                setTimeout(function() {
-                    removeLi.parentNode.removeChild(removeLi);
-                }, 600);
-            };
-            event.stopPropagation;
-        });
+        span[i].addEventListener("click", fadeOut);
     };
 };
 
+function fadeOut() {
+    if(event.target.localName === "span") {
+        var removeLi = event.target.parentNode;
+        // fadeout css
+        removeLi.classList.add("fadeOut");
+        // wait 600ms then remove li
+        setTimeout(function() {
+            removeLi.parentNode.removeChild(removeLi);
+        }, 600);
+    };
+};
+
+// Linethrough toggle
 function done() {
     for (var i = 0; i < li.length; i++) {
-        li[i].addEventListener("click", function() {
-            this.classList.toggle("linethrough");
-        });
+        li[i].addEventListener("click", linethrough);
     };
 }
 
+function linethrough() {
+    this.classList.toggle("linethrough");
+}
