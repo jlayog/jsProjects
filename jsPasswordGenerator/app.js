f5484fca7905285373eb1f5b13602768a167f0f3
jlayog <35618625+jlayog@users.noreply.github.com>
2019-11-21 14:27:55 -0500

    back up

---
+++ jsPasswordGenerator/app.js
@@ -0,0 +1,92 @@
+const resultElement = document.getElementById('result');
+const lengthElement = document.getElementById('length');
+const lowercaseElement = document.getElementById('lowercase');
+const uppercaseElement = document.getElementById('uppercase');
+const numberElement = document.getElementById('numbers');
+const symbolElement = document.getElementById('symbols');
+const generateElement = document.getElementById('generate');
+const clipboardElement = document.getElementById('clipboard');
+
+const randomFunc = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumber,
+    symbol: getRandomSymbol
+};
+
+clipboardElement.addEventListener('click', () => {
+    const textarea = document.createElement('textarea');
+    const password = resultElement.innerText;
+
+    if(!password) { return; }
+
+    textarea.value = password;
+	document.body.appendChild(textarea);
+	textarea.select();
+	document.execCommand('copy');
+	textarea.remove();
+	alert('Password copied to clipboard');
+})
+
+// Generate event listener
+
+generateElement.addEventListener('click', () => {
+    const length = +lengthElement.value;
+    const hasLower = lowercaseElement.checked;
+    const hasUpper = uppercaseElement.checked;
+    const hasNumber = numberElement.checked;
+    const hasSymbol = symbolElement.checked;
+
+    resultElement.innerText = generatePassword(
+        hasLower, 
+        hasUpper, 
+        hasNumber, 
+        hasSymbol, 
+        length);
+});
+
+// Generator functions - http://www.net-comber.com/charset.html - for all charset codes
+
+function generatePassword(lower, upper, number, symbol, length) {
+    // Init password var
+    let generatedPass = "";
+    
+
+    // Filter out checked types
+    const typesCount = lower + upper + number + symbol;
+    const typesArr = [{ lower }, { upper }, { number }, { symbol }]
+        .filter(item => Object.values(item)[0]);
+    
+    if(typesCount === 0) {
+        return '';
+    }
+
+    for(let i = 0; i < length; i += typesCount) {
+        typesArr.forEach(type => {
+            const funcName = Object.keys(type)[0];
+
+            generatedPass += randomFunc[funcName]();
+        });
+    }
+
+    const finalPassword = generatedPass.slice(0, length);
+
+    return finalPassword;
+}
+
+
+function getRandomLower() { 
+    return String.fromCharCode(Math.floor(Math.random() * 26 + 97)) 
+};
+function getRandomUpper() { 
+   return String.fromCharCode(Math.floor(Math.random() * 26 + 65)) 
+};
+function getRandomNumber() { 
+    return +String.fromCharCode(Math.floor(Math.random() * 10 + 48)) 
+};
+function getRandomSymbol() {
+    const symbols = '!@#$%^&*(){}<>/,.';
+    return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+
